ba0c5eb1cc273c81c376663709ffbcfcf76e2018
wotnak <wotnak@pm.me>
2025-09-23 17:07:16 +0200

    feat(compileCss): use standard css layers for base tailwind styles  (#17)

---
+++ src/assets.ts
@@ -0,0 +1,16 @@
+/**
+ * @see https://github.com/tailwindlabs/tailwindcss/blob/v4.1.13/packages/%40tailwindcss-browser/src/assets.ts
+ */
+import index from "tailwindcss-v4/index.css";
+import preflight from "tailwindcss-v4/preflight.css";
+import theme from "tailwindcss-v4/theme.css";
+import utilities from "tailwindcss-v4/utilities.css";
+
+const css = {
+  index,
+  preflight,
+  theme,
+  utilities,
+};
+
+export default { css };
--- src/types/tailwindcss-v4.d.ts
+++ src/types/tailwindcss-v4.d.ts
@@ -1,9 +1,19 @@
-declare module "tailwindcss-v4/theme.css" {
+declare module "tailwindcss-v4/index.css" {
   const content: string;
   export default content;
 }
 
 declare module "tailwindcss-v4/preflight.css" {
+  const content: string;
+  export default content;
+}
+
+declare module "tailwindcss-v4/theme.css" {
+  const content: string;
+  export default content;
+}
+
+declare module "tailwindcss-v4/utilities.css" {
   const content: string;
   export default content;
 }
